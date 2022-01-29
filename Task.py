a1ad99f7a4a854269e5d506137390fc28937f028
adhyak <adhya126@gmail.com>
2022-01-29 10:24:19 -0800

    List Read Write Implementation

--- Task.py
+++ Task.py
@@ -4,16 +4,24 @@ class Task:
     self.Date= Date
     self. Category=Category
     self. PriorityLevel= PriorityLevel
-    self.TASK_FILE='tasks'
-  
-   def Add(self):
-       f=open(self.TASK_FILE, 'w+')
+
+   def Add(self, filename):
+       f=open(filename, 'a+')
+       f.write("\n")
+       f.write("Task Name:")
        f.write(self.TaskName)
+       f.write("\n")
+       f.write("Due date is:")
        f.write(str(self.Date))
+       f.write("\n")
+       f.write("Category:")
        f.write(self.Category)
+       f.write("\n")
+       f.write("Priority Level:")
        f.write(self.PriorityLevel)
-
-
+       f.write("\n")
+       f.write("\n")
+       
    def Remind(self):
        currentdate=date.today()
        if(self.Date-currentdate<=1):
--- ToDoList.py
+++ ToDoList.py
@@ -1,19 +1,48 @@
-from datetime import date 
-import Priority
-from Task import Task 
+import datetime
+
+from Task import Task
 
 class toDoList:
     def __init__(self, Tasks, ListName):
         self.Tasks=Tasks
         self.ListName= ListName
 
-listname = str(input())
-TaskName=str(input())
-Date= int(input())
-Category= str(input())
-PriorityLevel= str(input())
-task= Task(Date, Category, PriorityLevel, TaskName)
-tasks=[]
-tasks.append(task)
-task.Add()
-todolist=toDoList(tasks, listname)
+print("Would you like to see a previous list? [y/n]")
+PrevListResponse=str(input())
+if(PrevListResponse=="y"):
+    print("What is the name of the list?")
+    ListNameResponse=str(input())
+    f=open(ListNameResponse, 'r')
+    for line in f:    
+        print(line)
+
+print("Would you like to create a new list? [y/n]")
+ListResponse=str(input())
+if ListResponse=="y":
+    print("What is the name of the list?")
+    listname = str(input())
+    tasks=[]
+    print("Would you like to add a task? [y/n]")
+    TaskResponse=str(input())
+    while TaskResponse=="y":
+        print ("What's the name of the task?")
+        TaskName=str(input())
+        print("When do want this to be done by?(Month number)")
+        Month=int(input())
+        print("What date in the month?")
+        Day= int(input())
+        print("What year?")
+        Year=int(input())
+        print("What hour?")
+        Hour=int(input())
+        Date=datetime.datetime(Year, Month, Day, Hour)
+        print("What category would you like to place this in? ex: hw, science project, etc.")
+        Category= str(input())
+        print("How important is this task to finish? (High/moderate/low)")
+        PriorityLevel= str(input())
+        task= Task(Date, Category, PriorityLevel, TaskName)
+        tasks.append(task)
+        task.Add(listname)
+        print("Would you like to add another task? [y/n]")
+        TaskResponse=str(input())
+    todolist=toDoList(tasks, listname)
